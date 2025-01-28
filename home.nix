9843199c2cad7ff1678fe2e36b98555817e14dcf
Poularde <poularde@msn.com>
2025-01-28 12:08:23 +0100

    nettoyage

--- home.nix
+++ home.nix
@@ -62,7 +62,3 @@
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "25.05";
 }
-# home-manager.users.poularde = { pkgs, ... }: {
-#     # home.packages = [ pkgs.atool pkgs.httpie ];
-#     
-# }
