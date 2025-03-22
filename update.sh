45fac8080c41ecc77efa4106ef703897ffa80b8f
Poularde <poularde@msn.com>
2025-03-22 15:06:00 +0100

    update flake

--- update.sh
+++ update.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/git
-nix flake update
-git add .
-git commit -m "update flake"
-git push
-nixos-rebuild switch --upgrade
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/git
+sleep 5
+sudo nix flake update
+sleep 10
+git add . && git commit -m "update flake" && git push
+sleep 5
+sudo nixos-rebuild switch --upgrade
