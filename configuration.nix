0e1f91e2d15f5f8152469d35b8bbb9ec325f44c4
Poularde <poularde@msn.com>
2025-11-09 12:15:07 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -135,6 +135,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     lutris
+    cmake
     # gnome-tweaks
     # gnome-shell-extensions
     libva-utils
