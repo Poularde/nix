a1136ead0bbaf4f5e67940bea52ae65dc528e661
Poularde <poularde@msn.com>
2025-11-15 16:17:27 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -209,7 +209,8 @@
   # accessible via `nvidia-settings`.
   nvidiaSettings = true;
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  package = config.boot.kernelPackages.nvidiaPackages.production;
+  package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    version = "580.65.06";
   };
 
   # ZSH
