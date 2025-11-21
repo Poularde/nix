19a277c2a95b19f8cd8db0be8213ca76f4a37c56
Poularde <poularde@msn.com>
2025-11-21 16:34:15 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -214,7 +214,7 @@
     sha256_64bit = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
     sha256_aarch64 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
     openSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
-    settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+    settingsSha256 = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
     persistencedSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
     }; 
   };
