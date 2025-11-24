923271ed3aeb7bfa5658a787d90b168bfd068942
Poularde <poularde@msn.com>
2025-11-24 09:12:00 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -209,16 +209,15 @@
   # accessible via `nvidia-settings`.
   nvidiaSettings = true;
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #version = "580.82.09";
-    version = "570.207";
-    #sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
-    sha256_64bit = "sha256-LWvSWZeWYjdItXuPkXBmh/i5uMvh4HeyGmPsLGWJfOI=";
-    sha256_aarch64 = "";
-    openSha256 = "";
-    settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
-    persistencedSha256 = "";
-    }; 
+  # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+  #   version = "580.82.09";
+  #   sha256_64bit = "sha256-Puz4MtouFeDgmsNMKdLHoDgDGC+QRXh6NVysvltWlbc=";
+  #   sha256_aarch64 = "";
+  #   openSha256 = "";
+  #   settingsSha256 = "sha256-um53cr2Xo90VhZM1bM2CH4q9b/1W2YOqUcvXPV6uw2s=";
+  #   persistencedSha256 = "";
+  #   };
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.latest; # Same as production 
   };
 
   # ZSH
