1a626d304b87d8f07fd451a01fc8ed284440fab5
Poularde <poularde@msn.com>
2025-12-10 19:14:45 +0100

    update flake

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,9 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Linux Kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   networking.hostName = "garuda"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
