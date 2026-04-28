05677ba7bf7d81f9d2c186b9a29041ad16e0c7b6
Poularde <poularde@msn.com>
2026-04-28 10:08:29 +0200

    update flake

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 eval "$(ssh-agent -s)" && ssh-add ~/.ssh/git
 sudo nix flake update
 git add . && git commit -m "update flake" && git push
-sudo nixos-rebuild switch --upgrade
+sudo nixos-rebuild switch
 sudo nix-collect-garbage --delete-older-than 30d
